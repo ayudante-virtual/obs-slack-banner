33243bb3b42778103dc63e9638c00a980fe98ed8
seblaz <sebastian.blazquez96@gmail.com>
2021-03-14 15:09:09 -0300

    Front end

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState, Fragment} from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        // maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
+
+const App = () => {
+    const classes = useStyles();
+    const baseUrl = `http://${process.env.REACT_APP_WEBAPP_HOSTNAME}:${process.env.REACT_APP_WEBAPP_PORT}/messages`
+    const [messages, setMessages] = useState([])
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            axios.get(baseUrl)
+                .then(({data: newMessages}) => {
+                    setMessages(newMessages)
+                })
+        }, 1000)
+        return () => clearInterval(timer)
+    }, [baseUrl])
+
+    const onMessageClick = (index) => {
+        const message = messages[index]
+        axios.put(`${baseUrl}/latest`, message)
+    }
+
+    return (
+        <div className={classes.root}>
+            <List component="nav">
+                <Divider/>
+                {messages.map(({text}, n) =>
+                    <Fragment key={n}>
+                        <ListItem button onClick={() => onMessageClick(n)}>
+                            <ListItemText primary={text}/>
+                        </ListItem>
+                        <Divider/>
+                    </Fragment>
+                )}
+            </List>
+        </div>
+    );
 }
 
-export default App;
+export default App
--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,21 +1,43 @@
 const redis = require('redis')
 const express = require("express")
+const cors = require('cors')
 const MessagesController = require("./controllers/MessagesController")
 const MessagesRepo = require("./infra/MessagesRepo")
 
-const redisClient = redis.createClient({
-    host: process.env.REDIS_HOSTNAME
-})
+/**
+ * Logger
+ * @type {Console}
+ */
+const logger = console
 
+/**
+ * Redis client
+ * @type {RedisClient}
+ */
+const redisClient = redis.createClient({host: process.env.REDIS_HOSTNAME})
 
+/**
+ * Express app
+ * @type {*|Express}
+ */
 const app = express()
 app.use(express.json());
+app.use(cors({origin: JSON.parse(process.env.CORS_DOMAINS)}))
 
+/**
+ * Repositories
+ * @type {module.MessagesRepo}
+ */
 const messagesRepo = new MessagesRepo(redisClient)
 
-const logger = console
+/**
+ * Controllers
+ */
 new MessagesController(logger, app, messagesRepo)
 
+/**
+ * Start app.
+ */
 const port = process.env.WEBAPP_PORT || 3000
 app.listen(port, () => {
     logger.info(`Webapp listening at port ${port}`)
