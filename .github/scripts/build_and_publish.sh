e1d587ca2ae8d51093078c2d99f826dd337f7233
seblaz <sebastian.blazquez96@gmail.com>
2021-03-14 16:13:26 -0300

    Fix image version

--- .github/scripts/build_and_publish.sh
+++ .github/scripts/build_and_publish.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -e
+
 docker build "$SRC_PATH" -t "$IMAGE_NAME:latest" --target=production
-docker tag "$IMAGE_NAME:latest" "$IMAGE_NAME:$VERSION"
+docker tag "$IMAGE_NAME:latest" "$IMAGE_NAME:$IMAGE_VERSION"
 echo "$REGISTRY_PASSWORD" | docker login $REGISTRY_URL -u $REGISTRY_USERNAME --password-stdin
 docker push $IMAGE_NAME:$IMAGE_VERSION
 docker push $IMAGE_NAME:latest
