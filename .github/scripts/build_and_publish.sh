645f143fc7b4082427d672e07d26501a16c28a7b
seblaz <sebastian.blazquez96@gmail.com>
2021-03-14 21:47:16 -0300

    Prod config

--- .github/scripts/build_and_publish.sh
+++ .github/scripts/build_and_publish.sh
@@ -3,6 +3,6 @@ set -e
 
 docker build "$SRC_PATH" -t "$IMAGE_NAME:latest" --target=production
 docker tag "$IMAGE_NAME:latest" "$IMAGE_NAME:$IMAGE_VERSION"
-echo "$REGISTRY_PASSWORD" | docker login $REGISTRY_URL -u $REGISTRY_USERNAME --password-stdin
-docker push $IMAGE_NAME:$IMAGE_VERSION
-docker push $IMAGE_NAME:latest
+echo "$REGISTRY_PASSWORD" | docker login "$REGISTRY_URL" -u "$REGISTRY_USERNAME" --password-stdin
+docker push "$IMAGE_NAME:$IMAGE_VERSION"
+docker push "$IMAGE_NAME:latest"
