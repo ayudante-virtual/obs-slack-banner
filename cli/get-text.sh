b8716e846e1fdc1654a2c3a2038e0ce93be9bae1
seblaz <sebastian.blazquez96@gmail.com>
2021-03-25 16:04:30 -0300

    Add get text cmd

--- cli/get-text.sh
+++ cli/get-text.sh
@@ -3,11 +3,17 @@ set -e
 
 FILE_PATH="${1:-text.txt}"
 echo "Text will be written to ${FILE_PATH}"
+OLD_CONTENT=$(cat "$FILE_PATH")
 
 function fetch() {
   echo -ne "Fetching latest text..."
-  curl -s https://obs-slack-banner.ayudantes.ninja/api/messages/latest | jq -r '.text' > "$FILE_PATH"
+  NEW_CONTENT=$(curl -s https://obs-slack-banner.ayudantes.ninja/api/messages/latest | jq -r '.text')
   echo -ne "OK"\\r
+
+  if [[ "$OLD_CONTENT" != "$NEW_CONTENT" ]]; then
+    echo "$NEW_CONTENT" > "$FILE_PATH"
+    OLD_CONTENT=NEW_CONTENT
+  fi
 }
 
 while true; do
